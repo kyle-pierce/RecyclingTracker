ca6f22bf0bdb79026cb87029b76590e7e7712384
Jacob Ward <jacobmward23@gmail.com>
2017-10-21 22:28:20 -0700

    Add new activity

---
+++ RecyclingTracker/app/src/main/java/com/example/dylanjergens/recyclingtracker/SelectMaterial.java
@@ -0,0 +1,16 @@
+package com.example.dylanjergens.recyclingtracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jacob on 10/21/2017.
+ */
+public class SelectMaterial extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_material);
+    }
+}
