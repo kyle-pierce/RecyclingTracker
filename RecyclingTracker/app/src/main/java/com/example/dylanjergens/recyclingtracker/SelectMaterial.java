9b63214c0b9a229bd0b1d9e143e48f1f0f5acbc5
Jacob Ward <jacobmward23@gmail.com>
2017-10-21 23:00:25 -0700

    Starter code to switch activities

--- RecyclingTracker/app/src/main/java/com/example/dylanjergens/recyclingtracker/SelectMaterial.java
+++ RecyclingTracker/app/src/main/java/com/example/dylanjergens/recyclingtracker/SelectMaterial.java
@@ -1,16 +1,40 @@
 package com.example.dylanjergens.recyclingtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Jacob on 10/21/2017.
  */
-public class SelectMaterial extends AppCompatActivity{
+public class SelectMaterial extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_material);
+    }
+
+    public void selectedPaper()
+    {
+        handleSelection("Paper");
+    }
+
+    public void selectedPlastic()
+    {
+        handleSelection("Plastic");
+    }
+
+    public void selectedGlass()
+    {
+        handleSelection("Glass");
+    }
+
+    public void handleSelection(String material)
+    {
+        Intent intent = new Intent(getApplicationContext(), SelectQuantity.class);
+        intent.putExtra("material", material);
+        startActivity(intent);
     }
 }
---
+++ RecyclingTracker/app/src/main/java/com/example/dylanjergens/recyclingtracker/SelectQuantity.java
@@ -0,0 +1,17 @@
+package com.example.dylanjergens.recyclingtracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jacob on 10/21/2017.
+ */
+public class SelectQuantity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_material);
+    }
+}
